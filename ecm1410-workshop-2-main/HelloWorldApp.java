06bfdf173ba542f27512383e091afff38df6f9fc
ArabellaN527 <an710@exeter.ac.uk>
2026-01-23 13:25:28 +0000

    Section 1

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,7 +1,12 @@
 // HelloWorld application
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World! My name is " + args[0] + ".");
+        System.out.print("Hello World! My name is ");
+        System.out.print(args[0]);
+        System.out.print(". ");
+        System.out.print("I am ");
+        System.out.print(args[1]);
+        System.out.println(" years old.");
     }
 }
 
